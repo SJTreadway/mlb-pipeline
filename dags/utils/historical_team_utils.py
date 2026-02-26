e2394ce917504a2f02b338c29ab8e7b3f619576c
Steven Treadway <treadway08@gmail.com>
2026-02-26 17:27:02 -0600

    feat: adds get_season_game_logs helper function. will create dag next

--- dags/utils/historical_team_utils.py
+++ dags/utils/historical_team_utils.py
@@ -7,15 +7,14 @@ Helper functions for historical_team_batting_logs and historical_team_pitching_l
 from __future__ import annotations
 
 import logging
-from datetime import datetime
 
 import pandas as pd
-from pybaseball import team_game_logs, cache
+from pybaseball import team_game_logs, season_game_logs, cache
 
 logger = logging.getLogger(__name__)
 
 # 2020 was shortened due to COVID - July 23 to Sept 27.
-SEASONS = list(range(1980, datetime.now().year + 1))
+SEASONS = list[int](range(1980, 2026))
 
 TEAMS = [
   "FLA", "LAA", "CAL", "MON",
@@ -31,7 +30,7 @@ TEAMS = [
 
 cache.enable()
 
-def get_game_data_by_team(team, stat_type) -> pd.DataFrame:
+def get_game_data_by_team(team, stat_type):
     game_df = pd.DataFrame()
     for year in SEASONS:
         temp_df = pd.DataFrame()
@@ -41,6 +40,25 @@ def get_game_data_by_team(team, stat_type) -> pd.DataFrame:
             temp_df['Season'] = year
             game_df = pd.concat((game_df, temp_df))
             logger.info(f'Game {stat_type} data loaded for {team} in {year}')
-        except RuntimeError as error:
-            logger.info(f'Unable to load game {stat_type} data for {team} in {year}')
+        except Exception as error:
+            logger.info(f'Unable to load game {stat_type} data for {team} in {year} with error: {error}')
     return game_df
+
+def get_season_game_logs():
+    event_df = pd.DataFrame()
+
+    for year in SEASONS:
+        try:
+            temp_df = season_game_logs(year)
+            temp_df['season'] = year
+            event_df = pd.concat((event_df, temp_df))
+            print(f'Event data loaded for {year}')
+        except ValueError as error:
+            print(error)
+
+    # add additional columns to dataframe
+    event_df['run_diff'] = event_df['home_score'].copy() - event_df['visiting_score'].copy()
+    event_df['home_victory'] = (event_df['run_diff'] > 0).astype(int)
+    event_df['run_total'] = event_df['home_score'].copy() + event_df['visiting_score'].copy()
+    event_df['date_dblhead'] = (event_df['date'].copy().astype(str) + event_df['game_num'].copy().astype(str)).astype(int)
+    return event_df
