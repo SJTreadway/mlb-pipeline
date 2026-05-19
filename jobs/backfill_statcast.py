4f4f3e236c68ee896984fe871d011f070dcdf96f
SJTreadway <treadway08@gmail.com>
2026-05-19 00:37:23 -0500

    feat: adds backfill action

--- jobs/backfill_statcast.py
+++ jobs/backfill_statcast.py
@@ -47,8 +47,8 @@ logging.basicConfig(
 log = logging.getLogger(__name__)
 
 MLB_API = "https://statsapi.mlb.com/api/v1"
-START_YEAR = 2015
-END_YEAR = 2025
+START_YEAR = int(os.environ.get('START_YEAR', 2015))
+END_YEAR   = int(os.environ.get('END_YEAR', 2025))
 MIN_PA = 50  # minimum plate appearances to qualify
 MIN_GS = 5  # minimum games started for pitchers
 API_SLEEP = 0.2  # seconds between API calls
